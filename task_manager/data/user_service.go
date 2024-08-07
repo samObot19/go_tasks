499fca26e1954a20d3fdf3cc71cd63812e5abaf6
samObot19 <125662080+samObot19@users.noreply.github.com>
2024-08-07 06:48:55 -0700

    Task 6

--- task_manager/data/user_service.go
+++ task_manager/data/user_service.go
@@ -16,14 +16,6 @@ func (s *Services) GetUser(username *string) (models.User, error) {
     return user, nil
 }
 
-// func (s *Services) GetAllUsers() ([]models.User, error){
-// 	users, err := s.Connection.ReadAllUsers()
-//     if err != nil {
-//         return []models.User{}, err
-//     }
-//     return users, nil
-
-// }
 
 func (s *Services) PromoteUser(username string) error{
     return s.Connection.ChangeRoleToAdmin(username)
@@ -40,10 +32,6 @@ func (s *Services) AddUser(user *models.User) error{
     }
 	return s.Connection.CreateUser(user)
 }
-
-// func (s *Services) RemoveUser(username *string) error {
-// 	return s.Connection.DeleteUser(*username)
-// }
 
 
 func (s *Services) UpdateUser(id *string, updatedUser *models.User) error {
